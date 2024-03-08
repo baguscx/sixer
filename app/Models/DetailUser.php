0e44aff20f5e273336892d28bbbed9064cc78d32
Bagus Cahyono <baguscy8@gmail.com>
2024-03-08 14:11:58 +0700

    create model table

---
+++ app/Models/DetailUser.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class DetailUser extends Model
+{
+    use HasFactory;
+    use softDeletes;
+
+    public $table = 'detail_user';
+
+    protected $dates = [
+        'updated_at',
+        'created_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'user_id',
+        'photo',
+        'role',
+        'contact_number',
+        'biography',
+        'updated_at',
+        'created_at',
+        'deleted_at',
+    ];
+}
