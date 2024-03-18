da47412bc7b640668f2486d240a767e74af59790
Bagus Cahyono <baguscy8@gmail.com>
2024-03-18 15:53:35 +0700

    feat(landing): backend landing

--- app/Http/Controllers/Landing/LandingController.php
+++ app/Http/Controllers/Landing/LandingController.php
@@ -3,7 +3,14 @@
 namespace App\Http\Controllers\Landing;
 
 use App\Http\Controllers\Controller;
+use App\Models\AdvantageService;
+use App\Models\AdvantageUser;
+use App\Models\Order;
+use App\Models\Service;
+use App\Models\Tagline;
+use App\Models\ThumbnailService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LandingController extends Controller
 {
@@ -12,7 +19,8 @@ class LandingController extends Controller
      */
     public function index()
     {
-        return view('sixer.pages.landing.index');
+        $services = Service::OrderBy('created_at', 'desc')->get();
+        return view('sixer.pages.landing.index', compact('services'));
     }
 
     /**
@@ -20,7 +28,7 @@ class LandingController extends Controller
      */
     public function create()
     {
-        //
+        abort(404);
     }
 
     /**
@@ -28,7 +36,7 @@ class LandingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        abort(404);
     }
 
     /**
@@ -36,7 +44,7 @@ class LandingController extends Controller
      */
     public function show(string $id)
     {
-        //
+        abort(404);
     }
 
     /**
@@ -44,7 +52,7 @@ class LandingController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        abort(404);
     }
 
     /**
@@ -52,7 +60,7 @@ class LandingController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        abort(404);
     }
 
     /**
@@ -60,23 +68,53 @@ class LandingController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        abort(404);
     }
 
     //custom function
     public function explore(){
-        return view('sixer.pages.landing.explore');
+        $services = Service::orderBy('created_at', 'desc')->get();
+        return view('sixer.pages.landing.explore', compact('servives'));
     }
 
     public function detail(string $id){
-        return view('sixer.pages.landing.detail');
+        $service = Service::where('id', $id)->first();
+        $thumbnail = ThumbnailService::where('service_id', $id)->get();
+        $advantage_service = AdvantageService::where('service_id', $id)->get();
+        $advantage_user = AdvantageUser::where('service_id', $id)->get();
+        $tagline = Tagline::where('service_id', $id)->get();
+
+        return view('sixer.pages.landing.detail', compact('service', 'thumbnail', 'advantage_service', 'advantage_user', 'tagline'));
     }
 
     public function booking(string $id){
+        $service = Service::where('id', $id)->first();
+        $user_buyer = Auth::user()->id;
+
+        //validation booking
+        if($service->users_id == $user_buyer){
+            toast()->warning('Sorry, you can not book your own service');
+            return back();
+        }
+
         //
+        $order = new Order;
+        $order->buyer_id = $user_buyer;
+        $order->freelancer_id = $service->user->id;
+        $order->service_id = $service->id;
+        $order->file = NULL;
+        $order->note = NULL;
+        $order->expired = Date('y-m-d', strtotime('+3 Days'));
+        $order->order_status_id = 4;
+        $order->save();
+
+        $order_detail = Order::where('id', $order->id)->first();
+
+        return redirect()->route('detail.booking.landing', $order->id);
     }
 
     public function detail_booking(string $id){
-        //
+        $order = Order::where('id', $id)->first();
+        return view('pages.landing.booking', compact($order));
     }
 }
